c616ca567353fc4b3425d2243cafd3f970ab2227
Mehekra734014 <80312287+Mehekra734014@users.noreply.github.com>
2021-07-05 14:09:30 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+	constructor(x,y,r){
+	var options = {
+		isStatic : false,
+		'restitution' : 0.3,
+		'friction': 0,
+		'density': 1.2
+	}
+	    
+		this.x=x;
+		this.y=y;
+		this.r=70;
+		this.body=Bodies.circle(x,y,(this.r-20)/2, options);
+		this.image = loadImage("paper.png");
+		World.add(world, this.body);
+
+	}
+	display(){
+		var pos =this.body.position;
+		var angle = this.body.angle;		
+		push()
+		translate(pos.x, pos.y);
+		rotate(angle);
+		imageMode(CENTER);
+    	image(this.image,0,0,this.r,this.r);
+		pop();
+	}
+
+};
